057c9e8655e664e6364daead3b2d3100a6e57378
fabianRamirez-sbs <fabian.ramirez@sbseguros.co>
2022-01-24 21:10:01 -0500

    feature/new data paga todoList

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home/home.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+  },
+  {
+    path: 'todoList',
+    component: TodoListComponent,
   },
 ];
 
--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoListService } from './../core/services/todo-list.service';
+import { TodoList } from '../core/models/todo-list';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.scss']
+  styleUrls: ['./todo-list.component.scss'],
 })
 export class TodoListComponent implements OnInit {
+  constructor(private todoListService: TodoListService) {}
 
-  constructor() { }
+  list: TodoList[] = [];
 
   ngOnInit(): void {
+    this.fetchProducts();
   }
-
+  fetchProducts() {
+    this.list = this.todoListService.getTodoList();
+  }
 }
